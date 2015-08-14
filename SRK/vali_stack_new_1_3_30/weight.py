c109d3a1af79ee9379440f803d4cacfd278b67d4
SudalaiRajkumar <ssraj.rox@gmail.com>
2015-08-14 17:14:43 +0530

    Create weight.py

---
+++ SRK/vali_stack_new_1_3_30/weight.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+from glob import glob
+import sys
+subname1=sys.argv[1]
+subname2=sys.argv[2]
+def myauc(y,pred):
+    fpr, tpr, thresholds = metrics.roc_curve(y, pred, pos_label=1)
+    return metrics.auc(fpr, tpr)
+
+sub1=pd.read_csv(subname1,index_col=0)
+sub2=pd.read_csv(subname2,index_col=0)
+print 'pred', sub1.shape
+subjects = range(1,13)
+real=[]
+for subject in subjects:
+    fnames =  ['../../../Data/train/subj%d_series%d_events.csv' % (subject,i) for i in range(7,9)]
+    for fname in fnames:
+        labels= pd.read_csv(fname,index_col=0)
+        real.append(labels)
+        print fname,labels.shape
+real=pd.concat(real)
+print 'combined', real.shape
+
+#yy=[4, 4, 4, 3, 7, 6]  # vali1 first arg, vali3 second arg
+yy=[7, 6, 7, 7, 4, 4]  # vali1_3 first arg, vali30 second arg
+weight={}
+for c,name in enumerate(real.columns.values):
+    weight[name]=yy[c]
+xx=[]
+print weight
+for name in real.columns.values:
+    
+    y=np.array(real[name])
+    yr1=np.array(sub1[name])
+    yr2=np.array(sub2[name])
+
+    yr=yr1*weight[name]+yr2*(10-weight[name])
+    yr/=10
+    #print name,j,myauc(y,yr)
+    sub1[name]=yr
+print weight
+sub1.to_csv('vali1_3_30_new_sub.csv')
+#sub1.to_csv('vali3_1_sub.csv')
