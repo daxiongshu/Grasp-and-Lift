f055c7e3aeb84c9caff3661f55579278f12aa215
carl <aixueer4ever@gmail.com>
2015-08-26 15:43:00 -0400

    Update write_script.py

--- carl/bag/write_script.py
+++ carl/bag/write_script.py
@@ -1,7 +1,7 @@
 f=open('runcv.sh','w')
-for subject in range(1,13):
-    for clf in range(1,7):
-        for electrode in [1,3,25,32]
+for electrode in [i for i in range(2,33) if i not in [1,3,25,32]]:
+    for subject in range(1,13):
+        for clf in range(1,7):
             f.write('python trycv.py %d %d %d\n'%(subject,clf,electrode))
 
 
